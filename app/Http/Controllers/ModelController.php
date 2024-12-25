16659614a5022d4a6cf6206acc0a7c9c3c109ccf
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2024-12-25 18:04:18 +0500

    qwwq

--- app/Http/Controllers/ModelController.php
+++ app/Http/Controllers/ModelController.php
@@ -9,6 +9,7 @@ use App\Models\Size;
 use App\Models\SubModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ModelController extends Controller
 {
