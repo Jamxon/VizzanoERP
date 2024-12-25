6de0bcf397c76445c7d6681bf2281f60dbac600c
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2024-12-25 19:30:32 +0500

    werty2345

--- app/Http/Controllers/ModelController.php
+++ app/Http/Controllers/ModelController.php
@@ -104,7 +104,7 @@ class ModelController extends Controller
 
     public function update(Request $request, Models $models)
     {
-        dd($request->all());
+        dd($request->all(), $models);
         // `data` maydonini JSON sifatida dekodlash
         $data = json_decode($request->input('data'), true);
 
