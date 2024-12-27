9da2f41af4912bdac6d6f26e31f5a9fa0d52ee55
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2024-12-27 20:04:43 +0500

    sdds

--- app/Http/Controllers/TechnologController.php
+++ app/Http/Controllers/TechnologController.php
@@ -22,19 +22,26 @@ class TechnologController extends Controller
     }
     public function storeSpecification(Request $request): \Illuminate\Http\JsonResponse
     {
-        $request->validate([
-            '*.name' => 'required|string',
-            '*.submodel_id' => 'required|integer',
-            '*.specifications' => 'required|array',
-            '*.specifications.*.name' => 'required|string',
-            '*.specifications.*.code' => 'required|string',
-            '*.specifications.*.quantity' => 'required|integer|min:0',
-            '*.specifications.*.comment' => 'nullable|string',
-        ]);
+        $data = json_decode($request->getContent(), true);
 
-        $data = $request->all();
+        if (is_null($data)) {
+            return response()->json([
+                'message' => 'Invalid JSON format',
+            ], 400);
+        }
 
-        foreach ($data as $datum) {
+        $validatedData = validator($data, [
+            'data' => 'required|array',
+            'data.*.name' => 'required|string',
+            'data.*.submodel_id' => 'required|integer',
+            'data.*.specifications' => 'required|array',
+            'data.*.specifications.*.name' => 'required|string',
+            'data.*.specifications.*.code' => 'required|string',
+            'data.*.specifications.*.quantity' => 'required|integer|min:0',
+            'data.*.specifications.*.comment' => 'nullable|string',
+        ])->validate();
+
+        foreach ($validatedData['data'] as $datum) {
             $specificationCategory = SpecificationCategory::create([
                 'name' => $datum['name'],
                 'submodel_id' => $datum['submodel_id'],
@@ -51,24 +58,25 @@ class TechnologController extends Controller
             }
         }
 
-        if ($specificationCategory && $specifications) {
+        if (isset($specificationCategory) && isset($specifications)) {
             return response()->json([
-                'message' => 'Specifications and SpecificationCategory created successfully'
+                'message' => 'Specifications and SpecificationCategory created successfully',
             ], 201);
-        }elseif (!$specificationCategory) {
+        } elseif (!isset($specificationCategory)) {
             return response()->json([
-                'message' => 'SpecificationCategory error'
+                'message' => 'SpecificationCategory error',
             ], 404);
-        }elseif (!$specifications) {
+        } elseif (!isset($specifications)) {
             return response()->json([
-                'message' => 'Specifications error'
+                'message' => 'Specifications error',
             ], 404);
-        }else {
+        } else {
             return response()->json([
-                'message' => 'Something went wrong'
+                'message' => 'Something went wrong',
             ], 500);
         }
     }
+
 
     public function updateSpecification(Request $request, $id): \Illuminate\Http\JsonResponse
     {
