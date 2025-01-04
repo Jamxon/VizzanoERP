9cf7b4b11dfabcbd063c5f7dc457efbca5c066d4
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-04 20:05:53 +0500

    dsds

---
+++ app/Models/OrderGroup.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderGroup extends Model
+{
+    use HasFactory;
+
+    protected $table = 'order_groups';
+
+    protected $fillable = [
+        'order_id',
+        'submodel_id',
+        'group_id',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function submodel()
+    {
+        return $this->belongsTo(SubModel::class);
+    }
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+}
