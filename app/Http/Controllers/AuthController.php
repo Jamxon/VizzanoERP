ec28f016840257821b879ad5bd2e13d861ab9bb8
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-04 16:53:24 +0500

    dsds

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,7 +58,7 @@ class AuthController extends Controller
 
         $user = User::where('username', $request->username)->first();
 
-        if (!$user && !$this->checkDjangoPassword($request->password, $user->password) && ($user->employee->status == 'kicked')) {
+        if (!$user || !$this->checkDjangoPassword($request->password, $user->password) || ($user->employee->status == 'kicked')) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
