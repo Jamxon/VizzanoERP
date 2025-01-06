be3af71d2aa2ac501f27064bda8bc5ab45e676a0
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-06 10:23:50 +0500

    dsds

--- app/Http/Controllers/TechnologController.php
+++ app/Http/Controllers/TechnologController.php
@@ -370,7 +370,7 @@ class TechnologController extends Controller
     public function getTarificationByOrderModelId($orderModelId)
     {
         $orderSubModel = OrderSubModel::where('order_model_id', $orderModelId)->first();
-        $submodels = SubModel::where('id', $orderSubModel->submodel_id)->with('tarification_category')
+        $submodels = SubModel::where('id', $orderSubModel->submodel_id)->with('tarification_categories')
             ->get()
             ->makeHidden(['sizes', 'modelColors', 'specificationCategories']);
         return response()->json($submodels, 200);
