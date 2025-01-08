0d9f757474b8e589b0da12ac15c929016c263d01
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-08 18:54:56 +0500

    sdds

--- app/Http/Controllers/WarehouseController.php
+++ app/Http/Controllers/WarehouseController.php
@@ -47,7 +47,7 @@ class WarehouseController extends Controller
         }
 
         $warehouses = Warehouse::where('branch_id', $employee->branch_id)
-            ->with('stoks','stoks.item')
+            ->with('stoks','stoks.item','users.user')
             ->get();
 
         return response()->json($warehouses, 200);
