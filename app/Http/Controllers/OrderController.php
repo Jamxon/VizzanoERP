1318310b0c35b2fd83158728d5240dd077e2d58b
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-09 15:25:24 +0500

    dssd

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -133,7 +133,7 @@ class OrderController extends Controller
     public function getOrderWithPlan()
     {
         $orders = Order::where('status', 'active')
-            ->where('start_date', '<=', date_format(now()->addDays(3), 'Y-m-d'))
+            ->where('start_date', '<=', date_format(now()->addDays(3), 'Y-m-d').' 00:00:00')
             ->orderBy('start_date', 'desc')
             ->get();
 
