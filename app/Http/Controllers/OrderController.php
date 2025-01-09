1ac1d46f34f7ccf344bd323988dc522fa2a4e06b
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-09 15:08:21 +0500

    wdds

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -134,7 +134,7 @@ class OrderController extends Controller
         $orders = Order::where('status', 'active')
             ->where(function ($query) {
                 $query->where('start_date', '<=', now()->toDateString())
-                    ->orWhereBetween('start_date', [now()->toDateString(), now()->addDays(3)->toDateString()]);
+                    ->where('start_date', '>=', now()->addDays(3)->toDateString())
             })
             ->orderBy('start_date', 'asc')
             ->get();
