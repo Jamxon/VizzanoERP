256c8fb0e10f0fa22b7a8d65552b4cc86f8c975f
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-09 19:21:50 +0500

    ds

--- app/Http/Controllers/WarehouseController.php
+++ app/Http/Controllers/WarehouseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Warehouse;
 use App\Models\WarehouseRelatedUser;
 use Illuminate\Http\Request;
@@ -53,6 +54,25 @@ class WarehouseController extends Controller
         return response()->json($warehouses, 200);
     }
 
+    public function getWarehouseUsers()
+    {
+        $employee = auth()->user()->employee;
 
+        if (!$employee) {
+            return response()->json(['message' => 'Employee not found'], 404);
+        }
+
+        // Employee modelidan branch_id ni olish
+        $branchId = $employee->branch_id;
+
+        // branch_id orqali foydalanuvchilarni filtrlash
+        $warehouses = User::where('role_id', 3)
+            ->whereHas('employee', function ($query) use ($branchId) {
+                $query->where('branch_id', $branchId);
+            })
+            ->get();
+
+        return response()->json($warehouses, 200);
+    }
 
 }
