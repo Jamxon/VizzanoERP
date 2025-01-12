7eb25787a05b5279374e4f7d79b1c4954af9a8e3
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-12 17:48:01 +0500

    sdsdds

--- app/Http/Controllers/ConstructorController.php
+++ app/Http/Controllers/ConstructorController.php
@@ -12,6 +12,8 @@ class ConstructorController extends Controller
         $orders = Order::where('status', 'active')
             ->whereDate('start_date', '<=', now()->addDays(3)->toDateString())
             ->orderBy('start_date', 'asc')
+            ->with('orderModels.model', 'orderModels.submodels', 'orderModels.submodels.size', 'orderModels.submodels.modelColor')
+            ->setHidden(['orderModels.model.submodels'])
             ->get();
 
         return response()->json($orders);
