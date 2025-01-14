8b44605b64a059e9c40a921dc12b1750aab7ea31
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-14 13:00:38 +0500

    dsds

---
+++ app/Http/Resources/OrderPrintingTime.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderPrintingTime extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "quantity" => $this->quantity,
+            "status" => $this->status,
+            "start_date" => $this->start_date,
+            "end_date" => $this->end_date,
+            "order_printing_times" => OrderPrintingTime::collection($this->orderPrintingTimes),
+        ];
+    }
+}
