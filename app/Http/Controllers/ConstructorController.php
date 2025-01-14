bdf3b43aa74a5ce12fc438aed7c6d0e3d5aba3a9
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-14 13:00:38 +0500

    dsds

--- app/Http/Controllers/ConstructorController.php
+++ app/Http/Controllers/ConstructorController.php
@@ -13,19 +13,6 @@ class ConstructorController extends Controller
     {
 
         $plannedTime = $request->input('planned_time') ?? now()->toDateString();
-//        $constructorOrders = [];
-//        $orderPrintingTimes = OrderPrintingTimes::whereDate('planned_time', $plannedTime)
-//            ->orderBy('planned_time', 'asc')
-//            ->with('orderModel.model', 'orderModel.submodels', 'orderModel.submodels.size', 'orderModel.submodels.modelColor')
-//            ->get();
-//
-//        foreach ($orderPrintingTimes as $order) {
-//            $orders = Order::where('id',$order->orderModel->order_id)
-//                ->with('orderModels.orderPrintingTimes')
-//                ->get();
-//            $constructorOrders[] = $orders;
-//        }
-
         $orders = Order::whereHas('orderModels.orderPrintingTimes', function ($query) use ($plannedTime) {
                 $query->whereDate('planned_time', $plannedTime);
             })
