ceb6f34d8ee95a11e923b0f5158822dcefe2b004
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-19 18:54:47 +0500

    sddsds

--- app/Http/Resources/OrderPrintingTime.php
+++ app/Http/Resources/OrderPrintingTime.php
@@ -21,31 +21,30 @@ class OrderPrintingTime extends JsonResource
             "status" => $this->status,
             "start_date" => $this->start_date,
             "end_date" => $this->end_date,
-            "order_printing_times" => $this->orderModel->flatMap(function ($orderModel) {
-                return $orderModel->orderPrintingTimes->map(function ($orderPrintingTime) use ($orderModel) {
-                    return [
-                        "id" => $orderPrintingTime->id,
-                        "planned_time" => $orderPrintingTime->planned_time,
-                        "actual_time" => $orderPrintingTime->actual_time,
-                        "status" => $orderPrintingTime->status,
-                        "comment" => $orderPrintingTime->comment,
-                        "user" => $orderPrintingTime->user,
-                        "model" => $orderModel->model->makeHidden(['submodels']),
-                        "submodels" => $orderModel->submodels
-                            ->groupBy('submodel_id')
-                            ->map(function ($groupedSubmodels) {
-                                $firstSubmodel = $groupedSubmodels->first();
-                                return [
-                                    "id" => $firstSubmodel->id,
-                                    "submodel" => $firstSubmodel->submodel->makeHidden(['sizes', 'modelColors']),
-                                    "sizes" => $groupedSubmodels->pluck('size')->unique()->values(),
-                                    "modelColors" => $groupedSubmodels->pluck('modelColor')->unique()->values(),
-                                    "total_quantity" => $groupedSubmodels->sum('quantity'),
-                                ];
-                            })->values(),
-                    ];
-                });
-            }),
+            "order_printing_times" => $this->orderModel ? $this->orderModel->orderPrintingTimes->map(function ($orderPrintingTime) {
+                return [
+                    "id" => $orderPrintingTime->id,
+                    "planned_time" => $orderPrintingTime->planned_time,
+                    "actual_time" => $orderPrintingTime->actual_time,
+                    "status" => $orderPrintingTime->status,
+                    "comment" => $orderPrintingTime->comment,
+                    "user" => $orderPrintingTime->user,
+                    "model" => $this->orderModel->model->makeHidden(['submodels']),
+                    "submodels" => $this->orderModel->submodels
+                        ->groupBy('submodel_id')
+                        ->map(function ($groupedSubmodels) {
+                            $firstSubmodel = $groupedSubmodels->first();
+                            return [
+                                "id" => $firstSubmodel->id,
+                                "submodel" => $firstSubmodel->submodel->makeHidden(['sizes', 'modelColors']),
+                                "sizes" => $groupedSubmodels->pluck('size')->unique()->values(),
+                                "modelColors" => $groupedSubmodels->pluck('modelColor')->unique()->values(),
+                                "total_quantity" => $groupedSubmodels->sum('quantity'),
+                            ];
+                        })->values(),
+                ];
+            }) : [],
         ];
     }
+
 }
