6866982ac0c50945a689f72f160616ec2273a5ef
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-20 16:10:32 +0500

    ssdds

--- app/Http/Controllers/CuttingMasterController.php
+++ app/Http/Controllers/CuttingMasterController.php
@@ -14,7 +14,7 @@ class CuttingMasterController extends Controller
         $orders = Order::where('status', 'active')
             ->whereDate('start_date', '<=', now()->addDays(15)->toDateString())
             ->orderBy('start_date', 'asc')
-            ->with('orderModel.model', 'orderModel.submodels', 'orderModel.submodels.size', 'orderModels.submodels.modelColor')
+            ->with('orderModel.model', 'orderModel.submodels', 'orderModel.submodels.submodel')
             ->get();
 
         $orders->each(function ($order) {
