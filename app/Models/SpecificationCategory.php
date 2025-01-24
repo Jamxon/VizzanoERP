b3e36000e8f3aa2da04b98c1eef954c0bab20f67
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-24 15:16:11 +0500

    sdsdsd

--- app/Models/SpecificationCategory.php
+++ app/Models/SpecificationCategory.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static whereIn(string $string, array $submodelIds)
+ */
 class SpecificationCategory extends Model
 {
     use HasFactory;
