984da2abc30f2c92407e661a73b997fb333a3334
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-24 15:27:46 +0500

    dssds

--- app/Http/Controllers/CuttingMasterController.php
+++ app/Http/Controllers/CuttingMasterController.php
@@ -231,7 +231,7 @@ class CuttingMasterController extends Controller
         }
     }
 
-    public function getSpecificationByOrderId($id): \Illuminate\Http\JsonResponse
+    public function getSpecificationByOrderId($id)
     {
         $order = Order::find($id);
 
