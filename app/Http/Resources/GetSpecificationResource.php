986244cb7802d2f16801ae5b335892a5e9037b08
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-24 15:45:05 +0500

    sddssd

--- app/Http/Resources/GetSpecificationResource.php
+++ app/Http/Resources/GetSpecificationResource.php
@@ -5,6 +5,11 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property mixed $orderModel
+ * @property mixed $name
+ * @property mixed $id
+ */
 class GetSpecificationResource extends JsonResource
 {
     /**
@@ -17,33 +22,31 @@ class GetSpecificationResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'orderModel' => $this->orderModel->map(function ($orderModel) {
-                return [
-                    'id' => $orderModel->id,
-                    'model' => $orderModel->model,
-                    'submodels' => $orderModel->submodels->map(function ($submodel) {
-                        return [
-                            'id' => $submodel->id,
-                            'name' => $submodel->name,
-                            'specificationCategories' => $submodel->specificationCategories->map(function ($specificationCategory) {
-                                return [
-                                    'id' => $specificationCategory->id,
-                                    'name' => $specificationCategory->name,
-                                    'specifications' => $specificationCategory->specifications->map(function ($specification) {
-                                        return [
-                                            'id' => $specification->id,
-                                            'name' => $specification->name,
-                                            'code' => $specification->code,
-                                            'quantity' => $specification->quantity,
-                                            'comment' => $specification->comment,
-                                        ];
-                                    }),
-                                ];
-                            }),
-                        ];
-                    }),
-                ];
-            }),
+            'orderModel' => $this->orderModel ? [
+                'id' => $this->orderModel->id,
+                'model' => $this->orderModel->model,
+                'submodels' => $this->orderModel->submodels->map(function ($submodel) {
+                    return [
+                        'id' => $submodel->id,
+                        'name' => $submodel->name,
+                        'specificationCategories' => $submodel->specificationCategories->map(function ($specificationCategory) {
+                            return [
+                                'id' => $specificationCategory->id,
+                                'name' => $specificationCategory->name,
+                                'specifications' => $specificationCategory->specifications->map(function ($specification) {
+                                    return [
+                                        'id' => $specification->id,
+                                        'name' => $specification->name,
+                                        'code' => $specification->code,
+                                        'quantity' => $specification->quantity,
+                                        'comment' => $specification->comment,
+                                    ];
+                                }),
+                            ];
+                        }),
+                    ];
+                }),
+            ] : null,
         ];
     }
 }
