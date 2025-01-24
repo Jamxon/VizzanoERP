b3646961b8f4cdfbaf7c9737d206f1191dfeadf3
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-24 15:36:41 +0500

    dsdsds

---
+++ app/Http/Resources/GetSpecificationResource.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GetSpecificationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'orderModel' => $this->orderModel->each(function ($orderModel) {
+                return [
+                    'id' => $orderModel->id,
+                    'model' => $orderModel->model,
+                    'submodels' => $orderModel->submodels->each(function ($submodel) {
+                        return [
+                            'id' => $submodel->id,
+                            'submodel' => $submodel->submodel->each(function ($submodel) {
+                                return [
+                                    'id' => $submodel->id,
+                                    'name' => $submodel->name,
+                                    'specificationCategories' => $submodel->specificationCategories->each(function ($specificationCategory) {
+                                        return [
+                                            'id' => $specificationCategory->id,
+                                            'name' => $specificationCategory->name,
+                                            'specifications' => $specificationCategory->specifications->each(function ($specification) {
+                                                return [
+                                                    'id' => $specification->id,
+                                                    'name' => $specification->name,
+                                                    'code' => $specification->code,
+                                                    'quantity' => $specification->quantity,
+                                                    'comment' => $specification->comment,
+                                                ];
+                                            }),
+                                        ];
+                                    }),
+                                ];
+                            }),
+                        ];
+                    })
+                ];
+            }),
+        ];
+    }
+}
