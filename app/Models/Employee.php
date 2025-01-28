c2ec5dfe92e21bbb20ba71d80955825d41e9b0c4
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-01-28 11:43:59 +0500

    yiyyyuyu

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static whereIn(string $string, $groupIds)
+ */
 class Employee extends Model
 {
     use HasFactory;
