2e6ef9cb75addb29878d7a974a54209cf5359a25
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-02-07 17:18:01 +0500

    dsdds

--- app/Http/Controllers/TailorMasterController.php
+++ app/Http/Controllers/TailorMasterController.php
@@ -10,7 +10,17 @@ class TailorMasterController extends Controller
     public function getOrders()
     {
         $orders = Order::where('branch_id' , auth()->user()->branch_id)
-            ->with('orderItems')
+            ->where('status', 'printing')
+            ->where('status', 'cutting')
+            ->where('status', 'tailoring')
+            ->with(
+                'orderModel',
+                'orderModel.model',
+                'orderModel.material',
+                'orderModel.sizes.size',
+                'orderModel.submodels.submodel',
+                'instructions'
+            )
             ->orderBy('created_at', 'desc')
             ->get();
     }
