d7d84aa5d68347c7a5153897f5991a5cdcc06983
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-02-07 16:32:37 +0500

    dsdsdssddsddssdsddsdsdsd

--- app/Http/Controllers/TailorMasterController.php
+++ app/Http/Controllers/TailorMasterController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class TailorMasterController extends Controller
 {
-    //
+    public function getOrders()
+    {
+        $orders = Order::where('branch_id' , auth()->user()->branch_id)
+            ->with('orderItems')
+            ->orderBy('created_at', 'desc')
+            ->get();
+    }
 }
