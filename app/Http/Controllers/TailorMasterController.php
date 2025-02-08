f4c46ae3378152cde245393d2f8deb1a0c77db5d
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-02-08 17:04:54 +0500

    dsds

--- app/Http/Controllers/TailorMasterController.php
+++ app/Http/Controllers/TailorMasterController.php
@@ -23,7 +23,7 @@ class TailorMasterController extends Controller
                 'orderModel.material',
                 'orderModel.sizes.size',
                 'orderModel.submodels.submodel',
-                'orderModel.submodels.group',
+                'orderModel.submodels.group.group',
                 'instructions'
             )
             ->orderBy('created_at', 'desc')
