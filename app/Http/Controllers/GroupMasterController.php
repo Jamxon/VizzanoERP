0dd029837ca343ed1902099e5e33b58593a6a944
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-02-09 12:49:19 +0500

    sdsd

--- app/Http/Controllers/GroupMasterController.php
+++ app/Http/Controllers/GroupMasterController.php
@@ -6,10 +6,25 @@ use Illuminate\Http\Request;
 
 class GroupMasterController extends Controller
 {
-    public function getOrders()
+    public function getOrders(): \Illuminate\Http\JsonResponse
     {
         $user = auth()->user();
-        $group = $user->group;
-        return $group->orders;
+
+        if (!$user->group) {
+            return response()->json(['message' => 'Group not found'], 404);
+        }
+
+        $orders = $user->group->orders()->with([
+            'orderModel',
+            'orderModel.model',
+            'orderModel.material',
+            'orderModel.sizes.size',
+            'orderModel.submodels.submodel',
+            'orderModel.submodels.group',
+            'instructions'
+        ])->get();
+
+        return response()->json($orders);
     }
+
 }
