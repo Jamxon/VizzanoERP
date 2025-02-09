3fed3c29b8315b58c142b92c3f3b71b53930d874
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-02-09 22:08:08 +0500

    fdfddf

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -12,18 +12,18 @@ class Department extends Model
     protected $fillable = ['name', 'responsible_user_id', 'branch_id'];
 
     protected $hidden = ['created_at', 'updated_at','branch_id','responsible_user_id'];
-    // Filialga aloqani o'rnatamiz
-    public function branch()
+
+    public function branch(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Branch::class);
     }
 
-    public function groups()
+    public function groups(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Group::class);
     }
 
-    public function responsibleUser()
+    public function responsibleUser(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class, 'responsible_user_id');
     }
