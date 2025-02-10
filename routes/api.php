00b9e31cd8db959b33b72724f3e7b6ff403f24ef
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-02-10 10:07:40 +0500

    sds
    dssd

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('groupMaster')->middleware('role:groupMaster')->group(function (){
    Route::get('orders',[\App\Http\Controllers\GroupMasterController::class, 'getOrders']);
+   Route::get('orders/show/{id}',[\App\Http\Controllers\GroupMasterController::class, 'showOrder']);
     Route::get('orders/{id}',[\App\Http\Controllers\GroupMasterController::class,'startOrder']);
     Route::get('employees',[\App\Http\Controllers\GroupMasterController::class, 'getEmployees']);
     Route::get('tarifications/{id}',[\App\Http\Controllers\GroupMasterController::class, 'getTarifications']);
