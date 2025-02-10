18f4c236d799e3e5ff045bf0ffc73abb52cc0da6
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-02-10 10:11:19 +0500

    dsdsds

--- app/Http/Resources/ShowOrderGroupMaster.php
+++ app/Http/Resources/ShowOrderGroupMaster.php
@@ -15,46 +15,47 @@ class ShowOrderGroupMaster extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->order->id,
-            'name' => $this->order->name,
-            'quantity' => $this->order->quantity,
-            'start_date' => $this->order->start_date,
-            'end_date' => $this->order->end_date,
-            'rasxod' => $this->order->rasxod,
-            'status' => $this->order->status,
-            'comment' => $this->order->comment,
+            'id' => $this->order->id ?? null,
+            'name' => $this->order->name ?? null,
+            'quantity' => $this->order->quantity ?? null,
+            'start_date' => $this->order->start_date ?? null,
+            'end_date' => $this->order->end_date ?? null,
+            'rasxod' => $this->order->rasxod ?? null,
+            'status' => $this->order->status ?? null,
+            'comment' => $this->order->comment ?? null,
             'orderModel' => [
-                'id' => $this->order->orderModel->id,
+                'id' => optional($this->order->orderModel)->id,
                 'model' => [
-                    'id' => $this->order->orderModel->model->id,
-                    'name' => $this->order->orderModel->model->name,
+                    'id' => optional($this->order->orderModel->model)->id,
+                    'name' => optional($this->order->orderModel->model)->name,
                 ],
                 'material' => [
-                    'id' => $this->order->orderModel->material->id,
-                    'name' => $this->order->orderModel->material->name,
+                    'id' => optional($this->order->orderModel->material)->id,
+                    'name' => optional($this->order->orderModel->material)->name,
                 ],
-                'sizes' => $this->order->orderModel->sizes->map(function ($size) {
-                    return [
-                        'id' => $size->id,
-                        'size' => $size->size,
-                        'quantity' => $size->quantity,
-                    ];
-                }),
-                'submodels' => $this->order->orderModel->submodels->map(function ($submodel) {
-                    return [
-                        'id' => $submodel->id,
-                        'submodel' => $submodel->submodel,
-                        'tarificationCategories' => $submodel->tarificationCategories->map(function ($category) {
-                            return [
-                                'id' => $category->id,
-                                'name' => $category->name,
-                                'tarifications' => $category->tarifications,
-                            ];
-                        }),
-                    ];
-                }),
+                'sizes' => optional($this->order->orderModel->sizes)->map(function ($size) {
+                        return [
+                            'id' => $size->id,
+                            'size' => $size->size,
+                            'quantity' => $size->quantity,
+                        ];
+                    }) ?? [],
+                'submodels' => optional($this->order->orderModel->submodels)->map(function ($submodel) {
+                        return [
+                            'id' => $submodel->id,
+                            'submodel' => $submodel->submodel,
+                            'tarificationCategories' => optional($submodel->tarificationCategories)->map(function ($category) {
+                                    return [
+                                        'id' => $category->id,
+                                        'name' => $category->name,
+                                        'tarifications' => $category->tarifications,
+                                    ];
+                                }) ?? [],
+                        ];
+                    }) ?? [],
             ],
-            'instructions' => $this->order->instructions,
+            'instructions' => $this->order->instructions ?? null,
         ];
     }
+
 }
