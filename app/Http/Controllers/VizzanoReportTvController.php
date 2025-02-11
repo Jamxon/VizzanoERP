3ff4484e8e27bbbfb428f4936b1b63792f6410f7
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-02-11 18:01:21 +0500

    fddfdf

--- app/Http/Controllers/VizzanoReportTvController.php
+++ app/Http/Controllers/VizzanoReportTvController.php
@@ -10,18 +10,17 @@ class VizzanoReportTvController extends Controller
 {
     public function getSewingOutputs(Request $request): \Illuminate\Http\JsonResponse
     {
-        $startDate = $request->get('start_date') ?? now()->subDays(6)->format('Y-m-d'); // Default: oxirgi 7 kun
-        $endDate = $request->get('end_date') ?? null; // Bugungi sana
-        $today = now()->format('Y-m-d'); // Bugungi sana
+        $startDate = $request->get('start_date') ?? now()->format('Y-m-d');
+        $endDate = $request->get('end_date');
+        $today = now()->format('Y-m-d');
 
-        $query = SewingOutputs::whereDate('created_at', '>=', $startDate)
-            ->whereDate('created_at', '<=', $endDate);
-
-        if ($endDate) {
-            $query->whereDate('created_at', '<=', $endDate);
+        if (!$endDate) {
+            $query = SewingOutputs::whereDate('created_at', '=', $startDate);
+        } else {
+            $query = SewingOutputs::whereDate('created_at', '>=', $startDate)
+                ->whereDate('created_at', '<=', $endDate);
         }
 
-        // Guruh va submodel bo'yicha umumiy va bugungi ishlab chiqarilgan miqdorlarni hisoblash
         $sewingOutputs = $query
             ->selectRaw('order_submodel_id, SUM(quantity) as total_quantity, SUM(CASE WHEN DATE(created_at) = ? THEN quantity ELSE 0 END) as today_quantity', [$today])
             ->groupBy('order_submodel_id')
@@ -29,12 +28,10 @@ class VizzanoReportTvController extends Controller
             ->orderBy('total_quantity', 'desc')
             ->get();
 
-        // Motivatsiyalarni olish
         $motivations = Motivation::all()->map(fn($motivation) => [
             'title' => $motivation->title,
         ]);
 
-        // Natijani shakllantirish
         $resource = [
             'sewing_outputs' => $sewingOutputs->map(function ($sewingOutput) {
                 return [
@@ -42,7 +39,7 @@ class VizzanoReportTvController extends Controller
                     'submodel' => $sewingOutput->orderSubmodel->submodel,
                     'group' => optional($sewingOutput->orderSubmodel->group)->group,
                     'total_quantity' => $sewingOutput->total_quantity,
-                    'today_quantity' => $sewingOutput->today_quantity, // Faqat bugungi natija
+                    'today_quantity' => $sewingOutput->today_quantity,
                 ];
             }),
             'motivations' => $motivations,
@@ -50,7 +47,4 @@ class VizzanoReportTvController extends Controller
 
         return response()->json($resource);
     }
-
-
-
 }
