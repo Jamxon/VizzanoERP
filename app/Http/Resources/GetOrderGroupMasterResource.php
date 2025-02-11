79b582299b41581924202d4589977311398c8268
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-02-11 16:13:02 +0500

    ddsds

--- app/Http/Resources/GetOrderGroupMasterResource.php
+++ app/Http/Resources/GetOrderGroupMasterResource.php
@@ -63,7 +63,6 @@ class GetOrderGroupMasterResource extends JsonResource
                                     ],
                                 ];
                             }),
-
                     ];
                 }),
             ],
