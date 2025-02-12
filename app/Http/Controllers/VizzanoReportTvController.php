537a88b5d30db746f652ce477847105c087952b2
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-02-12 19:23:48 +0500

    dds

--- app/Http/Controllers/VizzanoReportTvController.php
+++ app/Http/Controllers/VizzanoReportTvController.php
@@ -63,7 +63,7 @@ class VizzanoReportTvController extends Controller
 
         $resource = [
             'sewing_outputs' => $sewingOutputs->map(function ($sewingOutput) use ($employeeCounts, $workTimeByGroup) {
-                $group_id = optional($sewingOutput->orderSubmodel->group)->id;
+                $group_id = optional($sewingOutput->orderSubmodel->group->group)->id;
                 $employeeCount = $employeeCounts[$group_id] ?? 0;
                 $workTime = $workTimeByGroup[$group_id] ?? 0; // Ish vaqti soniyalarda
                 $submodelSpend = optional($sewingOutput->orderSubmodel->submodelSpend)->seconds ?? 0;
