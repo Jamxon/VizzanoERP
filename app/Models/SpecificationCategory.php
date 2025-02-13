97d70f4c766a8de1b92bd10d06c51b952a8561db
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-02-13 17:52:23 +0500

    dsdsds

--- app/Models/SpecificationCategory.php
+++ app/Models/SpecificationCategory.php
@@ -31,7 +31,7 @@ class SpecificationCategory extends Model
 
     public function submodel(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
-        return $this->belongsTo(OrderSubModel::class, 'specification_categories.submodel_id');
+        return $this->belongsTo(OrderSubModel::class, 'submodel_id');
     }
 
     public function specifications(): \Illuminate\Database\Eloquent\Relations\HasMany
