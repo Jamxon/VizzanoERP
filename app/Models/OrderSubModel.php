58a3195246a9870f0b264339fee8208e96ac39c6
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-02-19 15:04:48 +0500

    sddsdssdf

--- app/Models/OrderSubModel.php
+++ app/Models/OrderSubModel.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * @method static where(string $string, $orderModelId)
+ * @method static create(array $array)
  */
 class OrderSubModel extends Model
 {
