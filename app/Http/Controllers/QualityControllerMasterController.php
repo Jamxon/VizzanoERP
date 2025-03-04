0bfe09f06263269a358b10fd33f90743156ce1cd
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-03-04 15:06:26 +0500

    dsds

--- app/Http/Controllers/QualityControllerMasterController.php
+++ app/Http/Controllers/QualityControllerMasterController.php
@@ -79,7 +79,7 @@ class QualityControllerMasterController extends Controller
                     'model' => $orderSubModel->orderModel->model ?? null,
                     'qualityChecksTrue' => $counts[1] ?? 0, // Status true (1) bo'lganlar soni
                     'qualityChecksFalse' => $counts[0] ?? 0, // Status false (0) bo'lganlar soni
-                    'descriptions' => $descriptionCounts, // Tanlangan descriptionlar va soni
+                    'descriptions' => $orderSubModel->qualityChecks, // Tanlangan descriptionlar va soni
                 ];
             });
 
