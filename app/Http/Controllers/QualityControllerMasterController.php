97316e2f8bdc2cc7b84f646a46095ab589db8539
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-03-04 10:45:38 +0500

    dssd

--- app/Http/Controllers/QualityControllerMasterController.php
+++ app/Http/Controllers/QualityControllerMasterController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Department;
 use Illuminate\Http\Request;
 
 class QualityControllerMasterController extends Controller
 {
     public function result()
     {
-      return   $department = auth()->user();
+      return   $department = Department::where('responsible_user_id', auth()->id())->first();
         $groups = $department->groups;
         $employees = $groups->map(function ($group) {
             return $group->employees;
