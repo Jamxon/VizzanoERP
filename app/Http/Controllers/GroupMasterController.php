d7b74bdaf5a93f0a19c587f486a809fec95e2c6f
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-03-11 10:03:41 +0500

    dsdssd

--- app/Http/Controllers/GroupMasterController.php
+++ app/Http/Controllers/GroupMasterController.php
@@ -78,13 +78,6 @@ class GroupMasterController extends Controller
             ])
             ->first();
 
-
-        $totalQuantity = $order->orderModel->flatMap(function ($orderModel) {
-            return $orderModel->submodels->flatMap(function ($submodel) {
-                return $submodel->sewingOutputs;
-            });
-        })->sum('quantity');
-
         if (!$order) {
             return response()->json(['message' => 'Order not found'], 404);
         }
