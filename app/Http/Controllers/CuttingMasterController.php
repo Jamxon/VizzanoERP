314aa35da75a8039f09848beda92a95925537179
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-01 10:11:09 +0500

    dsdsds

--- app/Http/Controllers/CuttingMasterController.php
+++ app/Http/Controllers/CuttingMasterController.php
@@ -27,7 +27,7 @@ class CuttingMasterController extends Controller
             ->orderBy('start_date', 'asc')
             ->with(
                 'instructions',
-                'orderModel.model',
+//                'orderModel.model',
                 'orderModel.model.material',
                 'orderModel.submodels',
                 'orderModel.submodels.submodel',
