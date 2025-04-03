9bdc1796ba1ac8a6065807348491896a5a272eec
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-03 17:49:40 +0500

    sdsds

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -27,7 +27,7 @@ class Log extends Model
             'action' => $action,
             'old_data' => $oldData ? json_encode($oldData) : null,
             'new_data' => $newData ? json_encode($newData) : null,
-            'ip_address' => Request::getClientIp(),
+            'ip_address' => Request::getClientIps(),
             'user_agent' => Request::header('User-Agent'),
             'created_at' => now(),
         ]);
