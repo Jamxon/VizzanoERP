d3828922eabb73f94910df39127b0e231018b777
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-05 10:55:15 +0500

    dsds

--- app/Http/Controllers/TechnologController.php
+++ app/Http/Controllers/TechnologController.php
@@ -24,11 +24,11 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class TechnologController extends Controller
 {
-    public function showSpecificationCategory($id): \Illuminate\Http\JsonResponse
+    public function showSpecificationCategory($id)
     {
         $specificationCategory = SpecificationCategory::find($id)
             ->with('specifications');
-        dd($specificationCategory);
+        return $specificationCategory;
 
         if ($specificationCategory) {
             return response()->json($specificationCategory, 200);
