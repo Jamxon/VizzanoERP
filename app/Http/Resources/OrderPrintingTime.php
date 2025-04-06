9298be757c2990b2b5c12059f2675df50abcb0aa
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-06 12:23:47 +0500

    sdds

--- app/Http/Resources/OrderPrintingTime.php
+++ app/Http/Resources/OrderPrintingTime.php
@@ -32,19 +32,6 @@ class OrderPrintingTime extends JsonResource
                     'id' => $this->orderModel->material->id ?? 0,
                     'name' => $this->orderModel->material->name ?? null,
                 ],
-                'sizes' => $this->orderModel->sizes->map(function ($size) {
-                    return [
-                        'id' => $size->id,
-                        'size' => $size->size,
-                        'quantity' => $size->quantity,
-                    ];
-                }),
-                'submodels' => $this->orderModel->submodels->map(function ($submodel) {
-                    return [
-                        'id' => $submodel->id,
-                        'submodel' => $submodel->submodel,
-                    ];
-                }),
             ],
         ];
     }
