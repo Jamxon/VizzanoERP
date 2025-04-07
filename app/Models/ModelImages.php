144bd9f145f80d7bac2fe6e1155730b761d8ac90
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-07 14:29:22 +0500

    sddsd

--- app/Models/ModelImages.php
+++ app/Models/ModelImages.php
@@ -19,6 +19,15 @@ class ModelImages extends Model
 
     protected $hidden = ['created_at', 'updated_at', 'model_id'];
 
+    public function getImageAttribute($value): \Illuminate\Foundation\Application|string|\Illuminate\Contracts\Routing\UrlGenerator|\Illuminate\Contracts\Foundation\Application
+    {
+        if (filter_var($value, FILTER_VALIDATE_URL)) {
+            return $value;
+        }
+
+        return url('storage/' . $value);
+    }
+
     public function model()
     {
         return $this->belongsTo(Models::class);
