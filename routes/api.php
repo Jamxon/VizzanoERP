c48d92e56a395c972157106aae0723975b28f50b
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-09 15:55:40 +0500

    sdds

--- routes/api.php
+++ routes/api.php
@@ -231,7 +231,7 @@ Route::prefix('orderManager')->middleware('role:orderManager')->group(function (
     Route::patch('orders/change/{order}', [OrderController::class, 'changeOrderStatus']);
     Route::get('/orders/{id}/pdf', [OrderController::class, 'generateOrderPdf']);
 
-    
+
     Route::get('contragents', [OrderController::class, 'getContragents']);
 
     Route::get('models', [ModelController::class, 'index']);
