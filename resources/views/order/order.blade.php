5f800605d45c3f986016343a9131d120cf164287
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-09 17:01:00 +0500

    dssd

--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -152,7 +152,7 @@
             <div>
                 <h1 class="document-title">BUYURTMA HUJJATI</h1>
                 <div class="document-number">Hujjat № {order.id}</div>
-                <p>Sana: {{ $order.start_date }}</p>
+                <p>Sana: {{ $order }}</p>
                 <p>{{}}</p>
             </div>
         </div>
