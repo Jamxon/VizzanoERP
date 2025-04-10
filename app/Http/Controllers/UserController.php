3ca2999a961c5b46bf2138947b21fd1b45016e8d
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-10 18:44:08 +0500

    dsds

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
     {
         $user = Auth::user();
 
-          dd($employee = Employee::where('id', $user->employee_id)->first());
+          dd($employee = Employee::where('id', $user->employee->id)->first());
 
         $resource = new GetUserResource($employee);
 
