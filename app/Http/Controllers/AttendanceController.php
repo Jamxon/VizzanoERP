ab444b99aef4edc35cdce6fb96d7ce8cb7259a52
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-19 16:30:28 +0500

    sddsds

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\GetEmployeeResource;
 use App\Models\Attendance;
 use Illuminate\Http\Request;
 
@@ -11,6 +12,17 @@ class AttendanceController extends Controller
     {
         $date = $request->date ?? now()->toDateString();
         $attendances = Attendance::whereDate('date', $date)
+            ->orderBy('updated_at', 'desc')
+            ->get();
+
+        return response()->json($attendances);
+    }
+
+    public function getAttendanceHistory(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $date = $request->date ?? now()->toDateString();
+        $attendances = Attendance::where('date', $date)
+            ->with('employee')
             ->orderBy('updated_at', 'desc')
             ->get();
 
