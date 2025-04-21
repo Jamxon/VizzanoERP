b1f7c3412e0da8ba28d253a7fe5adc0610204237
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-21 13:17:23 +0500

    dsds

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -14,7 +14,7 @@ class ItemController extends Controller
     public function index(Request $request): \Illuminate\Http\JsonResponse
     {
         $query = strtolower($request->get('search'));
-        $type = $request->input('type');
+        $type = $request->input('type_id');
         $items = Item::where('branch_id', auth()->user()->employee->branch_id)
             ->where(function ($q) use ($query) {
                 $q->orWhereRaw('LOWER(name) LIKE ?', ["%$query%"])
