8b8669327e3a1aa0298418762bcc1a2250a8dd67
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-22 09:01:00 +0500

    dsds

--- app/Models/Source.php
+++ app/Models/Source.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @method static findOrFail($id)
  * @method static create(array $validated)
+ * @method static firstOrCreate(array $array)
  */
 class Source extends Model
 {
