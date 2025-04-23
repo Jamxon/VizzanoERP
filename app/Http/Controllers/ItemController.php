d72bf1af9401a45767f21c994de2fa22695e15fc
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-23 10:47:19 +0500

    ssd

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -108,7 +108,7 @@ class ItemController extends Controller
                     'comment' => $entry->comment,
                     'employee' => [
                         'id' => $entry->employee?->id,
-                        'full_name' => $entry->employee?->name,
+                        'name' => $entry->employee?->name,
                     ],
                     'quantity' => $entryItem->quantity,
                     'price' => $entryItem->price,
