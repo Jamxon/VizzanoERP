6ac17fa41c58022c44427785a1ecd5eb092440dd
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-04-24 15:36:15 +0500

    sfds

--- app/Http/Controllers/OrderImportController.php
+++ app/Http/Controllers/OrderImportController.php
@@ -154,6 +154,10 @@ class OrderImportController extends Controller
     {
         $file = $request->file('file');
 
+        if (!$file || !$file->isValid()) {
+            return response()->json(['success' => false, 'message' => "Fayl noto'g'ri yuklangan!"], 400);
+        }
+
         try {
             $spreadsheet = IOFactory::load($file->getPathname());
             $sheet = $spreadsheet->getActiveSheet();
