36cde0b224fc97f331a5ba0d7c90100c835fc1de
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-05-10 12:54:02 +0500

    dssd

--- app/Http/Controllers/WarehouseController.php
+++ app/Http/Controllers/WarehouseController.php
@@ -36,7 +36,7 @@ class WarehouseController extends Controller
                 'warehouse',
                 'order'
             ])
-            ->get();
+            ->paginate();
 
         return response()->json($balance);
     }
