3aabfee4fc8335b838656514048526a6df5dca72
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-05-12 10:47:35 +0500

    dsfdfssd

--- resources/views/pdf/stock-balances.blade.php
+++ resources/views/pdf/stock-balances.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Ombor Holati</title>
     <style>
-        body { font-family: sans-serif; font-size: 12px; }
+        body { font-family: DejaVu Sans, sans-serif; font-size: 12px; }
         table { width: 100%; border-collapse: collapse; margin-top: 20px; }
         th, td { border: 1px solid #444; padding: 5px; text-align: left; }
         th { background-color: #f2f2f2; }
