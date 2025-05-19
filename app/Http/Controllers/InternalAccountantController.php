c71ec61c0093f01ed02fdaf6953c043fd02ac3a0
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-05-19 19:27:59 +0500

    sdds

--- app/Http/Controllers/InternalAccountantController.php
+++ app/Http/Controllers/InternalAccountantController.php
@@ -170,7 +170,7 @@ class InternalAccountantController extends Controller
 
         $pdf = Pdf::loadView('pdf.daily-plan', [
             'plans' => $employeePlans
-        ])->setPaper([0, 0, 226.77, 141.73], 'portrait'); // 80mm x 50mm in points
+        ])->setPaper([0, 0, 141.73, 226.77], 'portrait'); // 80mm x 50mm in points
 
         return $pdf->download('daily_plan.pdf');
     }
