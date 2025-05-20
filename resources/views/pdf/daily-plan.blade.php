3950ed39a833cfcd8d165c8168126769b48878da
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-05-20 13:32:49 +0500

    sdds

--- resources/views/pdf/daily-plan.blade.php
+++ resources/views/pdf/daily-plan.blade.php
@@ -156,7 +156,7 @@
 
         <div class="barcode">
             <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG((string) $plan['plan_id'], 'C128', 1.5, 40) }}">
-            <hr style="margin-top: 10px">
+            <hr style="margin-top: 40px">
         </div>
     </div>
 @endforeach
