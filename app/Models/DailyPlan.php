5f2d52667eb26b032c82ad12a0cf7217844aa00a
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-05-21 10:00:34 +0500

    dsdsds

--- app/Models/DailyPlan.php
+++ app/Models/DailyPlan.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @method static create(array $array)
  * @method static where(string $string, mixed $dailyPlanId)
+ * @method static findOrFail(mixed $dailyPlanId)
  */
 class DailyPlan extends Model
 {
