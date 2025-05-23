5e251281af1f1d52a8e0cd78ddcf4f83d8035370
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-05-23 15:26:19 +0500

    fsse

--- app/Http/Controllers/InternalAccountantController.php
+++ app/Http/Controllers/InternalAccountantController.php
@@ -660,9 +660,9 @@ class InternalAccountantController extends Controller
         ]);
 
         if ($boxTarification->status === 'completed') {
-            return response()->json(['message' => '❌ Bu operatsiya allaqachon bajarilgan!'], 403);
+            return response()->json(['message' => '❌ Bu operatsiya allaqachon bajarilgan!'], 422);
         }elseif ($boxTarification->status === 'inactive') {
-            return response()->json(['message' => '❌ Bu operatsiya bekor qilingan!'], 403);
+            return response()->json(['message' => '❌ Bu operatsiya bekor qilingan!'], 422);
         }
 
         $boxTarification->update(['status' => 'completed']);
