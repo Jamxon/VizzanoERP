97dc8f2ef2de51f4a6f403fafad66f5923ed1a19
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-05-23 10:59:58 +0500

    dssd

---
+++ app/Models/BoxTarification.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BoxTarification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id', 'submodel_id', 'tarification_id', 'size_id',
+        'quantity', 'price', 'total', 'status'
+    ];
+
+    public function tarification(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Tarification::class);
+    }
+
+}
