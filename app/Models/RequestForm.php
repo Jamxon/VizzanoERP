ec5e71171e51b29b1549be6b962b900fdbf8f849
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-05-24 14:18:00 +0500

    dssdsd

---
+++ app/Models/RequestForm.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static create(array $array)
+ */
+class RequestForm extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'employee_id',
+        'currency_id',
+        'amount',
+        'purpose',
+        'comment',
+        'status',
+        'approved_by',
+        'approved_at',
+        'created_by',
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'approved_at' => 'datetime',
+    ];
+
+    public function employee(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function currency(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Currency::class);
+    }
+
+    public function creator(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function approver(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'approved_by');
+    }
+}
