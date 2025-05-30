33cb329485cdeacfa972f7c5247f95e76f3dcb6b
Jamshid monoblok <aliyevjamkhan499@gmail.com>
2025-05-30 20:28:00 +0500

    Add Telegram integration for issue reporting

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -52,5 +52,16 @@ if (!function_exists('transliterate')) {
         return strtr($text, $single);
     }
 
+    public static function sendMessage($chatId, $text)
+    {
+        $token = config('services.telegram.bot_token');
+        $url = "https://api.telegram.org/bot{$token}/sendMessage";
+
+        Http::post($url, [
+            'chat_id' => $chatId,
+            'text' => $text,
+            'parse_mode' => 'HTML',
+        ]);
+    }
 
 }
