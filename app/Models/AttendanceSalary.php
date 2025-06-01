9a058dd62e54bf80255e95baf1e9f008f52f821f
Jamshid monoblok <aliyevjamkhan499@gmail.com>
2025-06-01 16:08:28 +0500

    Remove unused SoftDeletes trait from AttendanceSalary model

--- app/Models/AttendanceSalary.php
+++ app/Models/AttendanceSalary.php
@@ -3,13 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class AttendanceSalary extends Authenticatable
 {
-    use SoftDeletes;
-
+    
     protected $table = "attendance_salary";
 
     protected $fillable = [
