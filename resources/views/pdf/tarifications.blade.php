4270e23fe8ed38fd1d64d5f19836f0278ef10b45
Jamshid monoblok <aliyevjamkhan499@gmail.com>
2025-06-11 17:52:21 +0500

    12345

--- resources/views/pdf/tarifications.blade.php
+++ resources/views/pdf/tarifications.blade.php
@@ -63,14 +63,13 @@
 
 <script type="text/php">
         $pdf->page_script('
-            if ($PAGE_COUNT > 1) {
                 $font = $fontMetrics->get_font("DejaVu Sans", "normal");
                 $size = 8;
                 $pageText = "Sahifa " . $PAGE_NUM . " / " . $PAGE_COUNT;
                 $x = 520;
                 $y = 820;
                 $pdf->text($x, $y, $pageText, $font, $size);
-            }
+
         ');
 </script>
 
