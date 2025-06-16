d7fe5f3276a4b063ba7d5ee8fc0c042f86d1455d
Jamshid monoblok <aliyevjamkhan499@gmail.com>
2025-06-16 11:15:28 +0500

    12345

--- app/Http/Resources/GetGroupsForResultCheckerResource.php
+++ app/Http/Resources/GetGroupsForResultCheckerResource.php
@@ -16,6 +16,36 @@ class GetGroupsForResultCheckerResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'name' => $this->name,
+            'responsibleUser' => [
+                'id' => $this->responsibleUser->employee->id,
+                'name' => $this->responsibleUser->employee->name,
+            ],
+            'orders' => $this->orders->map(function ($order) {
+                return [
+                    'id' => $order->id,
+                    'model' => [
+                        'id' => $order->orderSubmodel->orderModel->model->id,
+                        'name' => $order->orderSubmodel->orderModel->model->name,
+                    ],
+                    'submodel' => [
+                        'id' => $order->orderSubmodel->submodel->id,
+                        'name' => $order->orderSubmodel->submodel->name,
+                    ],
+                    'sewingOutputs' => $order->orderSubmodel->sewingOutputs
+                        ->whereBetween('created_at', [now()->startOfDay(), now()->endOfDay()])
+                        ->map(function ($sewingOutput) {
+                            return [
+                                'id' => $sewingOutput->id,
+                                'quantity' => $sewingOutput->quantity,
+                                'time' => [
+                                    'id' => $sewingOutput->time->id,
+                                    'time' => $sewingOutput->time->time,
+                                ],
+                            ];
+                        }),
+                ];
+            }),
         ];
     }
 }
