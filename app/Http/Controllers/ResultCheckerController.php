9e7d89df18f3313e2d9dcf70598a17939b4f2016
Jamshid monoblok <aliyevjamkhan499@gmail.com>
2025-06-16 14:45:59 +0500

    12345

--- app/Http/Controllers/ResultCheckerController.php
+++ app/Http/Controllers/ResultCheckerController.php
@@ -14,17 +14,21 @@ class ResultCheckerController extends Controller
     public function getGroups(Request $request): \Illuminate\Http\JsonResponse
     {
         $groups = Group::where('department_id', $request->input('department_id'))
+            ->whereHas('orders', function ($q) {
+                $q->whereIn('status', ['tailoring', 'pending', 'cutting']);
+            })
             ->with([
                 'responsibleUser',
-                'orders.orderSubmodel.submodel',
-                'orders.order' => function ($q) {
+                'orders' => function ($q) {
                     $q->whereIn('status', ['tailoring', 'pending', 'cutting']);
                 },
-                'orders.orderSubmodel.sewingOutputs'
+                'orders.orderSubmodel.submodel',
+                'orders.orderSubmodel.orderModel.model',
+                'orders.orderSubmodel.sewingOutputs' => function ($q) {
+                    $q->whereDate('created_at', now());
+                },
             ])
             ->get();
-
-
 
         $resource = GetGroupsForResultCheckerResource::collection($groups);
 
