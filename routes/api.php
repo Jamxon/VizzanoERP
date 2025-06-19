d751c95c051af113eba640f9fb16d388384e832c
Jamshid monoblok <aliyevjamkhan499@gmail.com>
2025-06-19 19:48:58 +0500

    12345

--- routes/api.php
+++ routes/api.php
@@ -385,6 +385,8 @@ Route::prefix('technologist')->middleware('role:technologist')->group(function (
     Route::get('lastTarification', [TechnologController::class, 'getLastTarification']);
     Route::get('nakladnoy', [InternalAccountantController::class, 'generateAttendanceNakladnoy']);
     Route::get('groups', [InternalAccountantController::class, 'getGroups']);
+    Route::get('departments', [SuperHRController::class, 'getDepartments']);
+
 });
 
 Route::prefix('constructor')->middleware('role:constructor')->group(function () {
