893669d533fb0b19af7d36cab9dd46f841868c45
Jamshid monoblok <aliyevjamkhan499@gmail.com>
2025-06-20 14:52:25 +0500

    12345

--- config/app.php
+++ config/app.php
@@ -187,6 +187,7 @@ return [
         // 'Example' => App\Facades\Example::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
+        'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
     ])->toArray(),
 
 ];
