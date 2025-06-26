fceabc6440b3ba489caedc1a6f2e0d0b1c887802
Jamshid monoblok <aliyevjamkhan499@gmail.com>
2025-06-26 11:18:54 +0500

    fix: update image URL handling in employee absence and holiday storage

--- app/Http/Controllers/SuperHRController.php
+++ app/Http/Controllers/SuperHRController.php
@@ -255,7 +255,7 @@ class SuperHRController extends Controller
                 'was_on_holiday' => $wasOnHoliday,
                 'was_on_absence' => $wasOnAbsence,
                 'comment' => $comment,
-                'image' => url($image) ?? null,
+                'image' => url('storage/' .$image) ?? null,
             ];
         });
 
