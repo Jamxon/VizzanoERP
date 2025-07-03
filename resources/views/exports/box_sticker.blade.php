609be43d9bd3b1323cf2c08beb0dc8ac314eebbf
Jamshid monoblok <aliyevjamkhan499@gmail.com>
2025-07-03 10:42:54 +0500

    fix: update table styles in box_sticker.blade.php for improved layout and consistency

--- resources/views/exports/box_sticker.blade.php
+++ resources/views/exports/box_sticker.blade.php
@@ -1,14 +1,12 @@
 @foreach ($stickers as $index => $sticker)
-    <table style="margin-bottom: 40px; border-collapse: collapse; width: 95%; border: 2px solid black;  font-family: DejaVu Sans, sans-serif;">
-        {{-- 1-4 qator: Logo va № --}}
+    <table style="margin-bottom: 40px; border-collapse: collapse; width: 100%; font-family: DejaVu Sans, sans-serif; border: 2px solid black;">
+        {{-- Logo and Sticker Number --}}
         <tr>
-            <td colspan="5" rowspan="4" style="text-align: center; border: 2px solid black;">
+            <td colspan="5" rowspan="4" style="text-align: center; border: 2px solid black; box-sizing: border-box;">
                 @php
-                    // Agar absolute path bo‘lsa, to‘g‘ridan-to‘g‘ri ishlatamiz
                     if (str_starts_with($imagePath, '/home')) {
                         $absoluteImagePath = $imagePath;
                     } else {
-                        // Nisbiy path bo‘lsa
                         $absoluteImagePath = storage_path('app/public/' . ltrim(str_replace('/storage/', '', $imagePath), '/'));
                     }
                 @endphp
@@ -18,49 +16,60 @@
                 @else
                     <strong>Logo yo‘q: {{ $absoluteImagePath }}</strong>
                 @endif
-
-
-
             </td>
-            <td colspan="2" rowspan="4" style="text-align: center; font-weight: bold; font-size: 55px; border: 2px solid black;">{{ $index + 1 }}</td>
+            <td colspan="2" rowspan="4" style="text-align: center; font-weight: bold; font-size: 55px; border: 2px solid black; box-sizing: border-box;">
+                {{ $index + 1 }}
+            </td>
         </tr>
         <tr></tr>
         <tr></tr>
         <tr></tr>
 
-        {{-- 5-6 qator: Submodel --}}
+        {{-- Submodel --}}
         <tr>
-            <td colspan="7" rowspan="2" style="text-align: center; font-weight: bold; font-size: 30px; border: 2px solid black;">
+            <td colspan="7" rowspan="2" style="text-align: center; font-weight: bold; font-size: 30px; border: 2px solid black; box-sizing: border-box;">
                 {{ $submodel ?? 'Submodel nomi yo‘q' }}
             </td>
         </tr>
-
         <tr></tr>
 
+        {{-- Art --}}
         <tr>
-            <td style="font-size: 25px; height: 60px; border: 2px solid black;"><p>Арт:</p></td>
-            <td colspan="6" style="font-weight: bold; text-align: center; font-size: 40px; height: 60px; border: 2px solid black;">{{ $model ?? '---' }}</td>
+            <td style="font-size: 25px; height: 60px; border: 2px solid black; box-sizing: border-box;">Арт:</td>
+            <td colspan="6" style="font-weight: bold; text-align: center; font-size: 40px; height: 60px; border: 2px solid black; box-sizing: border-box;">
+                {{ $model ?? '---' }}
+            </td>
         </tr>
 
+        {{-- Color --}}
         <tr>
-            <td style="font-size: 20px; height: 60px; border: 2px solid black;"><p>Цвет:</p></td>
-            <td colspan="6" style="font-weight: bold; text-align: center; font-size: 34px; height: 60px; border: 2px solid black;">{{ $sticker['color'] ?? '---' }}</td>
+            <td style="font-size: 20px; height: 60px; border: 2px solid black; box-sizing: border-box;">Цвет:</td>
+            <td colspan="6" style="font-weight: bold; text-align: center; font-size: 34px; height: 60px; border: 2px solid black; box-sizing: border-box;">
+                {{ $sticker['color'] ?? '---' }}
+            </td>
         </tr>
 
+        {{-- Table Headers --}}
         <tr>
-            <td colspan="3" style="text-align: center; border: 2px solid black;"><p>Размер</p></td>
-            <td colspan="4" style="text-align: center; border: 2px solid black;"><p>Количество</p></td>
+            <td colspan="3" style="text-align: center; border: 2px solid black; box-sizing: border-box;">Размер</td>
+            <td colspan="4" style="text-align: center; border: 2px solid black; box-sizing: border-box;">Количество</td>
         </tr>
 
+        {{-- Sizes from array --}}
         @foreach($sticker as $key => $row)
             @if(is_int($key) && is_array($row) && count($row) == 2 && is_string($row[0]))
                 <tr>
-                    <td colspan="3" style="text-align: center; font-size: 35px; font-weight: bold; height: 50px; border: 2px solid black;">{{ $row[0] }}</td>
-                    <td colspan="4" style="text-align: center; font-size: 35px; font-weight: bold; height: 50px; border: 2px solid black;">{{ $row[1] }}</td>
+                    <td colspan="3" style="text-align: center; font-size: 35px; font-weight: bold; height: 50px; border: 2px solid black; box-sizing: border-box;">
+                        {{ $row[0] }}
+                    </td>
+                    <td colspan="4" style="text-align: center; font-size: 35px; font-weight: bold; height: 50px; border: 2px solid black; box-sizing: border-box;">
+                        {{ $row[1] }}
+                    </td>
                 </tr>
             @endif
         @endforeach
 
+        {{-- Extra sizes from orderSizes --}}
         @php
             $sizes = [];
             foreach ($sticker as $key => $value) {
@@ -73,12 +82,15 @@
         @foreach ($sizes as $size)
             @foreach($size as $row)
                 <tr>
-                    <td colspan="3" style="text-align: center; font-size: 35px; font-weight: bold; height: 50px; border: 2px solid black;">{{ $row }}</td>
-                    <td colspan="4" style="text-align: center; font-size: 35px; font-weight: bold; height: 50px; border: 2px solid black;"></td>
+                    <td colspan="3" style="text-align: center; font-size: 35px; font-weight: bold; height: 50px; border: 2px solid black; box-sizing: border-box;">
+                        {{ $row }}
+                    </td>
+                    <td colspan="4" style="text-align: center; font-size: 35px; font-weight: bold; height: 50px; border: 2px solid black; box-sizing: border-box;"></td>
                 </tr>
-@endforeach
-@endforeach
+          @endforeach
+        @endforeach
 
+{{-- Net Weight & Gross Weight --}}
 @php
     $indexedItems = array_filter($sticker, fn($key) => is_int($key), ARRAY_FILTER_USE_KEY);
     $last = end($indexedItems);
@@ -87,12 +99,20 @@
 
         @if(is_array($last) && count($last) == 2 && is_numeric($last[0]) && is_numeric($last[1]))
             <tr>
-                <td colspan="3" style="text-align: center; font-weight: bold; font-size: 15px; border: 2px solid black;">Нетто (кг)</td>
-                <td colspan="4" style="text-align: center; font-weight: bold; font-size: 15px; border: 2px solid black;">Брутто (кг)</td>
+                <td colspan="3" style="text-align: center; font-weight: bold; font-size: 15px; border: 2px solid black; box-sizing: border-box;">
+                    Нетто (кг)
+                </td>
+                <td colspan="4" style="text-align: center; font-weight: bold; font-size: 15px; border: 2px solid black; box-sizing: border-box;">
+                    Брутто (кг)
+                </td>
             </tr>
             <tr>
-                <td colspan="3" style="text-align: center; font-size: 35px; font-weight: bold; height: 50px; border: 2px solid black;">{{ $last[0] }}</td>
-                <td colspan="4" style="text-align: center; font-size: 35px; font-weight: bold; height: 50px; border: 2px solid black;">{{ $last[1] }}</td>
+                <td colspan="3" style="text-align: center; font-size: 35px; font-weight: bold; height: 50px; border: 2px solid black; box-sizing: border-box;">
+                    {{ $last[0] }}
+                </td>
+                <td colspan="4" style="text-align: center; font-size: 35px; font-weight: bold; height: 50px; border: 2px solid black; box-sizing: border-box;">
+                    {{ $last[1] }}
+                </td>
             </tr>
         @endif
     </table>
