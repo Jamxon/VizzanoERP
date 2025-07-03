991b0bdc11f088380405e1f8d6c7573cb9d837c7
Jamshid monoblok <aliyevjamkhan499@gmail.com>
2025-07-03 10:29:21 +0500

    fix: update fallback message in box_sticker.blade.php to display image path when logo is missing

--- resources/views/exports/box_sticker.blade.php
+++ resources/views/exports/box_sticker.blade.php
@@ -10,7 +10,7 @@
                 @if(file_exists($absoluteImagePath))
                     <img src="{{ $absoluteImagePath }}" alt="Logo" style="width: 140px; height: auto;" />
                 @else
-                    <strong>Logo yo‘q</strong>
+                    <strong>$imagePath</strong>
                 @endif
 
             </td>
