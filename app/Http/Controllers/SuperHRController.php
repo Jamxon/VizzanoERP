3f79a875ecdce75ba07d0a98912d37ca8151977f
Jamshid monoblok <aliyevjamkhan499@gmail.com>
2025-07-11 15:45:14 +0500

    feat: add API endpoint to retrieve group plans with validation for month and year

--- app/Http/Controllers/SuperHRController.php
+++ app/Http/Controllers/SuperHRController.php
@@ -741,7 +741,7 @@ class SuperHRController extends Controller
                 $q->whereHas('user', fn($q) => $q->where('role_id', $filters['role_id']));
             });
 
-        $employees = $query->orderByDesc('id')->paginate(10);
+        $employees = $query->orderByDesc('updated_at')->paginate(10);
 
         return (new GetEmployeeResourceCollection($employees))->response();
     }
