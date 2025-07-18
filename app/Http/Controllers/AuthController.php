4d48aab58bd677381ca9d21ca5cc3b6f62df3a1b
Jamshid monoblok <aliyevjamkhan499@gmail.com>
2025-07-18 10:35:31 +0500

    feat: add API endpoint to retrieve group plans with validation for month and year

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -78,27 +78,7 @@ class AuthController extends Controller
             return response()->json(['error' => 'Foydalanuvchi ishdan chiqarilgan'], 401);
         }
 
-        if($user->role->name === null){
-            $token = JWTAuth::fromUser($user);
-
-            Log::add(
-                $user->id,
-                'Muvaffaqiyatli tizimga kirish',
-                'login',
-                null,
-                [
-                    'user' => $user,
-                ]
-            );
-
-            return response()->json([
-                'token' => $token,
-                'user' => $user
-            ]);
-        }
-
-        // 🔒 Qo‘shimcha shartlar: agar role 'tailor' bo‘lsa
-        if ($user->role->name === 'tailor') {
+        if (optional($user->role)->name === 'tailor') {
             $now = now();
 
             if ($now->hour >= 20) {
