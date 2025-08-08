8308513008d565403c5322a6b0f7466266879cac
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-08-08 13:54:01 +0500

    fix: add endpoint to retrieve model by tarification code with validation

--- app/Http/Controllers/SuperHRController.php
+++ app/Http/Controllers/SuperHRController.php
@@ -736,7 +736,7 @@ class SuperHRController extends Controller
                             ->whereRaw("EXTRACT(DOW FROM date) != 0");   // PostgreSQL’da 0 = yakshanba
                 }
             ])
-            ->orderByDesc('attendance_absent_count');
+            ->orderByDesc('attendance_absent_count - holidays_count');
 
         if (!empty($filters['search'])) {
             $search = strtolower($filters['search']);
