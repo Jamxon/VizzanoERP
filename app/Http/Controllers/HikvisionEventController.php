17e662bdb92e90178f597a1b72a3920ec4fbc108
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-08-09 14:03:49 +0500

    fix: add endpoint to retrieve model by tarification code with validation

--- app/Http/Controllers/HikvisionEventController.php
+++ app/Http/Controllers/HikvisionEventController.php
@@ -14,6 +14,17 @@ class HikvisionEventController extends Controller
     {
         $contentType = $request->header('Content-Type');
 
+        Log::add(
+            null,
+            'Hikvision event received',
+            'info',
+            null,
+            [
+                'content_type' => $contentType,
+                'request_data' => $request->all(),
+            ]
+        );
+
         if (str_contains($contentType, 'multipart/form-data')) {
             $eventLogRaw = $request->input('event_log');
             $image = $request->file('Picture');
