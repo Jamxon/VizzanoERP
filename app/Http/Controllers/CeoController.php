6132facd871328a7a6d294b090b536a9301d020d
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-08-25 15:54:35 +0500

    fix: simplify eager loading in CeoController by removing unused fields

--- app/Http/Controllers/CeoController.php
+++ app/Http/Controllers/CeoController.php
@@ -10,24 +10,46 @@ class CeoController extends Controller
     {
         $startDate = $request->start_date;
         $endDate = $request->end_date;
+
         $groups = \App\Models\Group::where('department_id', $request->department_id)
             ->with(['orders' => function ($query) use ($startDate, $endDate) {
-                $query->whereHas('order.orderModel.submodels.sewingOutputs', function ($query) use ($startDate, $endDate) {
-                    $query->whereBetween('created_at', [$startDate, $endDate]);
-                })->with(['order' => function ($query) {
-                    $query->select('id', 'name', 'quantity', 'status', 'start_date', 'end_date');
-                }, 'order.orderModel' => function ($query) {
-                    $query->select('id', 'order_id', 'model_id', 'rasxod', 'status', 'minute')
-                        ->with('model:id,name');
-                }, 'order.orderModel.submodels' => function ($query) {
-                    $query->select('id', 'order_model_id')
-                        ->with('sewingOutputs');
-                }]);
-            },
-                'responsibleUser.employee:id,name',])
+                $query->whereHas('order.orderModel.submodels.sewingOutputs', function ($q) use ($startDate, $endDate) {
+                    $q->whereBetween('created_at', [$startDate, $endDate]);
+                })
+                    ->with([
+                        'order' => function ($q) {
+                            $q->select('id', 'name', 'quantity', 'status', 'start_date', 'end_date');
+                        },
+                        'order.orderModel' => function ($q) {
+                            $q->select('id', 'order_id', 'model_id', 'rasxod', 'status', 'minute')
+                                ->with('model:id,name');
+                        },
+                        'order.orderModel.submodels' => function ($q) use ($startDate, $endDate) {
+                            $q->select('id', 'order_model_id')
+                                ->with(['sewingOutputs' => function ($sq) use ($startDate, $endDate) {
+                                    $sq->whereBetween('created_at', [$startDate, $endDate])
+                                        ->select(
+                                            'id',
+                                            'submodel_id',
+                                            'quantity',
+                                            'created_at'
+                                        );
+                                }])
+                                // qo‘shimcha aggregatlar
+                                ->withSum(['sewingOutputs as total_quantity' => function ($sq) use ($startDate, $endDate) {
+                                    $sq->whereBetween('created_at', [$startDate, $endDate]);
+                                }], 'quantity')
+                                ->withMin(['sewingOutputs as min_date' => function ($sq) use ($startDate, $endDate) {
+                                    $sq->whereBetween('created_at', [$startDate, $endDate]);
+                                }], 'created_at')
+                                ->withMax(['sewingOutputs as max_date' => function ($sq) use ($startDate, $endDate) {
+                                    $sq->whereBetween('created_at', [$startDate, $endDate]);
+                                }], 'created_at');
+                        }
+                    ]);
+            }, 'responsibleUser.employee'])
             ->get();
 
         return response()->json($groups);
-
     }
 }
