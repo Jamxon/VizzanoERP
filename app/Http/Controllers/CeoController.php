983a6329870a30ebbac0744eb9e6f823b2ea8040
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-08-25 15:42:02 +0500

    fix: optimize data retrieval in CeoController with eager loading

--- app/Http/Controllers/CeoController.php
+++ app/Http/Controllers/CeoController.php
@@ -11,9 +11,20 @@ class CeoController extends Controller
         $startDate = $request->start_date;
         $endDate = $request->end_date;
         $groups = \App\Models\Group::where('department_id', $request->department_id)
-            ->whereHas('orders.order.orderModel.submodels.sewingOutputs', function ($query) use ($startDate, $endDate) {
-                $query->whereBetween('created_at', [$startDate, $endDate]);
-            })
+            ->with(['orders' => function ($query) use ($startDate, $endDate) {
+                $query->whereHas('order.orderModel.submodels.sewingOutputs', function ($query) use ($startDate, $endDate) {
+                    $query->whereBetween('created_at', [$startDate, $endDate]);
+                })->with(['order' => function ($query) {
+                    $query->select('id', 'name', 'quantity', 'status', 'start_date', 'end_date');
+                }, 'order.orderModel' => function ($query) {
+                    $query->select('id', 'order_id', 'model_id', 'material_id', 'rasxod', 'status', 'minute')
+                        ->with('model:id,name,article')
+                        ->with('material:id,name,unit');
+                }, 'order.orderModel.submodels' => function ($query) {
+                    $query->select('id', 'order_model_id', 'name', 'quantity', 'status')
+                        ->with('sewingOutputs');
+                }]);
+            }])
             ->get();
 
         return response()->json($groups);
