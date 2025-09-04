7812c1a4b57078bda7d8ff9a52ddc3955e67b29a
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-09-04 12:57:33 +0500

    fix: enhance order data structure in CeoController by adding submodel name and outputs to submodels

--- routes/api.php
+++ routes/api.php
@@ -59,7 +59,7 @@ Route::prefix('ceo')->middleware('role:ceo')->group(function () {
     Route::get('departments', [SuperHRController::class, 'getDepartments']);
     Route::post('groups', [CasherController::class, 'getGroupsByDepartmentId']);
     Route::post('groups2', [CasherController::class, 'getGroupsOrdersEarnings']);
-    Route::get('groups2/excel', [CasherController::class, 'exportGroupsOrdersEarnings']);
+    Route::post('groups2/excel', [CasherController::class, 'exportGroupsOrdersEarnings']);
     Route::get('orders', [CasherController::class, 'getOrders']);
     Route::post('salaries', [CasherController::class, 'giveSalaryOrAdvance']);
     Route::get('employees/{employee}', [UserController::class, 'showEmployee']);
