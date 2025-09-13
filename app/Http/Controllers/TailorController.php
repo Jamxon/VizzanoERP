b6011065f2114d894a9352bcd3e824f606091f8c
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-09-13 09:50:39 +0500

    fix: enhance order data structure in CeoController by adding submodel name and outputs to submodels

--- app/Http/Controllers/TailorController.php
+++ app/Http/Controllers/TailorController.php
@@ -173,6 +173,9 @@ class TailorController extends Controller
                 $q->whereHas('order.orderModel.submodels.sewingOutputs', function ($query) use ($startDate, $endDate) {
                     $query->whereBetween('created_at', [$startDate, $endDate]);
                 })
+                    ->whereHas('order.orderModel.submodels.tarificationCategories.', function ($query) {
+                        $query->where('region', 'uz');
+                    })
                     ->orWhereDoesntHave('order.orderModel.submodels.sewingOutputs');
             })
             ->with([
