a719bf2807802489bee5d144d01c6b89f4610ff1
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-09-15 14:30:51 +0500

    fix: enhance order data structure in CeoController by adding submodel name and outputs to submodels

--- app/Exports/SummarySheet.php
+++ app/Exports/SummarySheet.php
@@ -184,11 +184,14 @@ class OrdersSheet implements FromArray, WithHeadings, WithTitle, ShouldAutoSize,
     public function headings(): array
     {
         return [
-            'Buyurtma ID', "Buyurtma nomi",'Model','Submodellari','Mas’ul','Narx USD','Narx so‘m',
-            'Umumiy qty','Rasxod limiti (so‘m)','Bonus','Tarifikatsiya','Ajratilgan transport','Ajratilgan AUP',
-            'Ajratilgan oylik xarajat','Daromad % xarajat','Amortizatsiya','Jami qo‘shimcha','Doimiy xarajat (so‘m)',
-            'Jami ishlab chiqarish tannarxi (so‘m)','Sof foyda (so‘m)','Bir dona mahsulot tannarxi (so‘m)',
-            'Bir dona foyda (so‘m)','Rentabellik %'
+            'Buyurtma ID', "Buyurtma nomi",'Model','Submodellari','Mas’ul',
+            'Narx USD','Narx so‘m','Umumiy qty','Rasxod limiti (so‘m)','Bonus','Tarifikatsiya',
+            'Ajratilgan transport','Ajratilgan AUP','Ajratilgan oylik xarajat',
+            'Daromad % xarajat','Amortizatsiya',
+            'Jami qo‘shimcha','Doimiy xarajat (so‘m)',
+            'Jami ishlab chiqarish tannarxi (so‘m)','Sof foyda (so‘m)',
+            'Bir dona mahsulot tannarxi (so‘m)','Bir dona foyda (so‘m)',
+            'Rentabellik %'
         ];
     }
 
