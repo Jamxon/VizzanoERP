fb443f05f5d0f55c06808e7540fffcd5e52fffc6
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-09-20 15:50:17 +0500

    fix: enhance order data structure in CeoController by adding submodel name and outputs to submodels

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -41,6 +41,7 @@ class UserController extends Controller
                     \DB::raw('SUM(etl.quantity * t.second) as total_seconds')
                 )
                 ->where('e.branch_id', $branchId)
+                ->where('payment_type', 'piece_work')
                 ->when($request->department_id, fn($q) => $q->where('e.department_id', $request->department_id))
                 ->when($request->group_id, fn($q) => $q->where('e.group_id', $request->group_id))
                 ->whereBetween('etl.date', [$request->start_date, $request->end_date])
