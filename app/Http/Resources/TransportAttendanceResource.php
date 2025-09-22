e59f642b3db337dc90bb0fe8e1c8e424817508b9
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-09-22 16:26:27 +0500

    fix: enhance order data structure in CeoController by adding submodel name and outputs to submodels

--- app/Http/Resources/TransportAttendanceResource.php
+++ app/Http/Resources/TransportAttendanceResource.php
@@ -24,6 +24,7 @@ class TransportAttendanceResource extends JsonResource
                 return [
                     'id'   => $this->transport->id,
                     'name' => $this->transport->name,
+                    'capacity' => $this->transport->capacity,
 
                     'employees' => $this->transport->dailyEmployees
                         ->filter(function ($daily) {
