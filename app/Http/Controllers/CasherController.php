e1b12ea695216163e7795fdbd568bc553da1c2a1
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-09-23 13:04:56 +0500

    fix: enhance order data structure in CeoController by adding submodel name and outputs to submodels

--- app/Http/Controllers/CasherController.php
+++ app/Http/Controllers/CasherController.php
@@ -1720,7 +1720,7 @@ class CasherController extends Controller
                 return [
                     'cashbox' => $tx->cashbox,
                     'type' => $tx->type,
-                    'amount' => number_format($tx->amount, 2, '.', ' '),
+                    'amount' => $tx->amount,
                     'currency' => $tx->currency,
                     'date' => $tx->date,
                     'source' => $tx->source,
