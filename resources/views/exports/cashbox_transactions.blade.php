74e11565cf3bc4ebc9e0162cac1d6d6ac3920920
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-09-26 11:33:34 +0500

    fix: enhance order data structure in CeoController by adding submodel name and outputs to submodels

--- resources/views/exports/cashbox_transactions.blade.php
+++ resources/views/exports/cashbox_transactions.blade.php
@@ -7,18 +7,11 @@
             </td>
         </tr>
 
-        {{-- Sarlavhalar --}}
-        <tr style="background: #4CAF50; color: white;">
-            <th style="border: 1px solid #000; padding: 5px; font-weight: bold; width: 100px">Sana</th>
-            <th style="border: 1px solid #000; padding: 5px; font-weight: bold; width: 300px">Summa</th>
-            <th style="border: 1px solid #000; padding: 5px; font-weight: bold; width: 100px">Komment</th>
-        </tr>
-
         {{-- Purpose bo'yicha transactionlar --}}
         @foreach($rows as $row)
             <tr>
                 <td style="border: 1px solid #000; padding: 3px; width: 100px;">{{ $row->date }}</td>
-                <td style="border: 1px solid #000; padding: 3px; width: 300px;">{{ $row->comment ?? '' }}</td>
+                <td style="border: 1px solid #000; padding: 3px; text-align: center; width: 300px;">{{ $row->comment ?? '' }}</td>
                 <td style="border: 1px solid #000; padding: 3px; width: 100px; text-align: right;">{{ number_format($row->amount, 0, '.', ' ') }}</td>
             </tr>
         @endforeach
@@ -26,7 +19,7 @@
         {{-- Purpose bo'yicha jami (sariq rangda) --}}
         <tr style="background: #FFFF99;">
             <td style="border: 1px solid #000; padding: 3px; width: 100px; font-weight: bold;">Jami</td>
-            <td style="border: 1px solid #000; padding: 3px; width: 300px; font-weight: bold; text-align: right;">{{ number_format($rows->sum('amount'), 0, '.', ' ') }}</td>
+            <td style="border: 1px solid #000; padding: 3px; width: 300px;  font-weight: bold; text-align: center;">{{ number_format($rows->sum('amount'), 0, '.', ' ') }}</td>
             <td style="border: 1px solid #000; padding: 3px; width: 100px;"></td>
         </tr>
 
