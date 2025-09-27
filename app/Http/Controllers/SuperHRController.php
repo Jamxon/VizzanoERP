da4c4502ac6b1aba4ecac639003f5b09f2cbfecb
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-09-27 11:17:47 +0500

    fix: enhance order data structure in CeoController by adding submodel name and outputs to submodels

--- app/Http/Controllers/SuperHRController.php
+++ app/Http/Controllers/SuperHRController.php
@@ -843,13 +843,14 @@ class SuperHRController extends Controller
                 true,
                 function ($q) use ($user, $filters) {
                     if ($user->role->name === 'groupMaster') {
-                        $q->where('group_id', $user->group_id);
+                        // group_id ni user->employee ichidan olish kerak
+                        $q->where('employees.group_id', $user->employee->group_id);
                     } elseif (!empty($filters['group_id'])) {
-                        $q->where('group_id', $filters['group_id']);
+                        $q->where('employees.group_id', $filters['group_id']);
                     }
                 }
             )
-//            ->when($filters['group_id'] ?? false, fn($q) => $q->where('group_id', $filters['group_id']))
+            //            ->when($filters['group_id'] ?? false, fn($q) => $q->where('group_id', $filters['group_id']))
             ->when($filters['status'] ?? false, fn($q) => $q->where('employees.status', $filters['status']))
             ->when($filters['type'] ?? false, fn($q) => $q->where('employees.type', $filters['type']))
             ->when($filters['role_id'] ?? false, function ($q) use ($filters) {
