69b683ec1125463616099b3270a9f6c916776c9b
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-09-29 17:46:42 +0500

    fix: enhance order data structure in CeoController by adding submodel name and outputs to submodels

--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -5,13 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Issue extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'user_id',
