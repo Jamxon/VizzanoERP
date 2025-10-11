767dd676d96afb05b82e26399b2b9240266161bb
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-10-11 10:51:14 +0500

    fix: update S3 storage path for employee images in SuperHRController

--- app/Http/Controllers/SuperHRController.php
+++ app/Http/Controllers/SuperHRController.php
@@ -1066,7 +1066,7 @@ class SuperHRController extends Controller
                 $filename = time() . '.' . $file->getClientOriginalExtension();
 
                 // S3 ga yuklaymiz
-                $path = $file->storeAs('images', $filename, 's3');
+                $path = $file->storeAs('employees', $filename, 's3');
 
                 Storage::disk('s3')->setVisibility($path, 'public');
                 $img = Storage::disk('s3')->url($path);
