f1981f7be1341d18e7e5c90975c64be9b5fb85e3
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-10-11 17:13:04 +0500

    fix: add Message model import in ChatController for proper functionality

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\{Chat, ChatUser, User};
+use App\Models\{Chat, ChatUser, User, Message};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
