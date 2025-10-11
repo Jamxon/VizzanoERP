64c7d5283db0ce44aa6ea33b924a7e6a15b64136
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-10-11 17:07:59 +0500

    fix: add missing Cache facade import in ChatController

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Cache;
 
 class ChatController extends Controller
 {
