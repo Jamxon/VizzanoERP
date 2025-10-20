07c3275178785bddd32025226fb17da2461359f7
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-10-20 15:26:26 +0500

    3223

--- app/Console/Commands/MonitoringReport.php
+++ app/Console/Commands/MonitoringReport.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
+use App\Models\User;
 
 class MonitoringReport extends Command
 {
