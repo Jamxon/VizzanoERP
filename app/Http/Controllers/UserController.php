fb5cf9bd2141474b71fee9ae446f4b167bbb4a34
Jamkhan Monoblock(Ziffler) <aliyevjamkhan499@gmail.com>
2025-10-25 14:14:09 +0500

    323

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\Employee;
 use App\Models\Log;
 use App\Models\User;
 use App\Models\Issue;
+use App\Models\SalaryChange;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
