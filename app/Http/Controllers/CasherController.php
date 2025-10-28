fe8a84737abd64d59097d53e9ab7386b336f2ae1
Jamkhan <121377571+Jamxon@users.noreply.github.com>
2025-10-28 20:40:57 +0500

    212121

--- app/Http/Controllers/CasherController.php
+++ app/Http/Controllers/CasherController.php
@@ -1002,7 +1002,7 @@ class CasherController extends Controller
             // ✅ Telegram xabarini tranzaksiya tugagandan keyin yuborish
             DB::afterCommit(function () use ($employee, $validated) {
                 $text = "💸 *To‘lov amalga oshirildi!*\n"
-                    . "🏢 Filial: " . auth()->user->employee->branch->name . "\n"
+                    . "🏢 Filial: " . auth()->user()->employee->branch->name . "\n"
                     . "👤 Xodim: {$employee->name}\n"
                     . "👤 Bajardi: " .auth()->user()->employee->name . "\n"
                     . "💰 Miqdor: " . number_format($validated['amount'], 0, '.', ' ') . " so‘m\n"
