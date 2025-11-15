e3751d985586efad935732e49cf154342afa677b
Jamxon <aliyevjamkhan499@gmail.com>
2025-11-15 12:14:47 +0500

    REdsazx

--- app/Http/Controllers/DailyPaymentController.php
+++ app/Http/Controllers/DailyPaymentController.php
@@ -939,7 +939,8 @@ class DailyPaymentController extends Controller
         $workingEmployees = Employee::where('branch_id', $branchId)
             ->when($departmentId, fn($q) => $q->where('department_id', $departmentId))
             ->where('status', 'working') // status filter
-            ->select('id', 'name')
+            ->select('id', 'name', 'position_id')
+            ->with('position:id,name')
             ->get();
 
         return response()->json([
